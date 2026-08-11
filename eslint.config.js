3f446e4669ff1fc0032895c6109ed2aae2506057
huangtao <huangtao@gooeto.com>
2026-08-11 23:59:56 +0800

    feat: 建立薄荷绿应用外壳

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import eslint from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist/**', 'node_modules/**', '.superpowers/**'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: { parser: tseslint.parser, extraFileExtensions: ['.vue'] },
+    },
+  },
+)
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="app-shell">
+    <section
+      class="app-shell__panel"
+      aria-labelledby="app-title"
+    >
+      <p class="app-shell__eyebrow">
+        梦幻西游工具箱
+      </p>
+      <h1 id="app-title">
+        门派任务工具
+      </h1>
+      <p class="app-shell__message">
+        应用正在搭建
+      </p>
+    </section>
+  </main>
+</template>
